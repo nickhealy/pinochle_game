ef79030f30d79e5c9307ff9061f88dc300e1eafc
Nick Healy <nickrhealy@gmail.com>
2022-08-02 21:28:21 +0100

    host in test app can connect to clients

--- client/test_app/index.js
+++ client/test_app/index.js
@@ -1,8 +1,11 @@
-function createPlayer(playerId) {
-  const container = document.createElement('div')
-  container.classList.add('player')
-  container.innerHTML=`
-    <h3>${playerId === 0 ? 'HOST' : 'Player ' + playerId}</h3>
+const peers = [];
+const connections = [];
+
+function createPlayer(playerIdx) {
+  const container = document.createElement("div");
+  container.classList.add("player");
+  container.innerHTML = `
+    <h3>${playerIdx === 0 ? "HOST" : "Player " + playerIdx}</h3>
     <div id='hand-container'>
       
     </div>
@@ -13,20 +16,53 @@ function createPlayer(playerId) {
       <button type='button' id='pass-btn'>Pass</button>
       <button type='button' id='hez-btn'>Hez</button>
     </div>
-    <div>
-      <button id='join-room'>Start</button>
-      <button id='leave-room'>End</button>
-    </div>
-  `
-  return container
+    ${
+      (playerIdx > 0 &&
+        `
+      <div>
+        <button class='network-join' id='join-room-${playerIdx}'>Start</button>
+        <button class='network-leave' id='leave-room-${playerIdx}'>End</button>
+      </div>
+      `) ||
+      ""
+    }
+  `;
+
+  const joinBtn = container.querySelector(`#join-room-${playerIdx}`);
+  if (joinBtn) {
+    joinBtn.addEventListener("click", () => connectHostToPeer(playerIdx));
+  }
+
+  // make the peer
+  const peer = new Peer(`peer-${playerIdx}`);
+  peer.on("connection", function (conn) {
+    console.log(`Peer ${playerIdx} has received connection from ${conn.peer}`);
+  });
+
+  peers.push(peer);
+
+  return container;
 }
 
 function createPlayers() {
   const players = [];
   for (let i = 0; i < 4; i++) {
-    players.push(createPlayer(i))
+    players.push(createPlayer(i));
   }
-  document.getElementById('player-container').append(...players)
+  document.getElementById("player-container").append(...players);
 }
 
-createPlayers()
+function connectHostToPeer(playerIdx) {
+  const host = peers[0];
+  const conn = host.connect(`peer-${playerIdx}`);
+  conn.on("open", () => {
+    console.log(`Host has opened connection to ${conn.peer}`);
+  });
+  connections.push(conn);
+
+  document
+    .querySelector(`#join-room-${playerIdx}`)
+    .setAttribute("disabled", true);
+}
+
+createPlayers();
