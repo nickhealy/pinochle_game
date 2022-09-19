6333e493aa02aaa4b580398a0b665dbe210f3f6c
Nicholas Healy <nickrhealy@gmail.com>
2022-09-19 19:51:16 +0100

    show player's names when teams are assigned

--- game/ui/scenes/game/Game.scene.ts
+++ game/ui/scenes/game/Game.scene.ts
@@ -1,22 +1,29 @@
-import "reflect-metadata";
 import { inject, injectable } from "inversify";
-import * as inversify from "inversify";
 import TYPES from "../../../inversify-types";
 import OtherPlayer from "./OtherPlayer";
 import OwnHand from "./OwnHand";
 import { OpponentPosition } from "./OtherHand";
+import { StoreType } from "../../store";
+import EventEmitter from "../../events/EventEmitter";
+import { LobbyEvents } from "../../events/events";
 
 @injectable()
 class GameScene {
   private ownHand: OwnHand;
+
   private otherPlayerWest: OtherPlayer;
   private otherPlayerNorth: OtherPlayer;
   private otherPlayerEast: OtherPlayer;
   private _container: HTMLDivElement;
+  private _store: StoreType;
+  private _ee: EventEmitter;
+  private ownId: string | null = null;
   constructor(
     @inject<OwnHand>(TYPES.OwnHand) ownHand: OwnHand,
     @inject(TYPES.OtherPlayerFactory)
-    otherPlayerFactory: (position: OpponentPosition) => OtherPlayer
+    otherPlayerFactory: (position: OpponentPosition) => OtherPlayer,
+    @inject(TYPES.Store) store: StoreType,
+    @inject(TYPES.EventEmitter) ee: EventEmitter
   ) {
     this.ownHand = ownHand;
     this.otherPlayerWest = otherPlayerFactory(OpponentPosition.WEST);
@@ -25,6 +32,44 @@ class GameScene {
     this._container = document.getElementById(
       "gameplay-container"
     ) as HTMLDivElement;
+    this._store = store;
+    this._ee = ee;
+
+    this.initializeSubscriptions();
+  }
+
+  private initializeSubscriptions() {
+    this._store.subscribe("ownId", (id) => {
+      this.ownId = id;
+    });
+    this._ee.addEventListener(LobbyEvents.TEAMS_RECEIVED, () => {
+      this.assignPlayerIds();
+    });
+  }
+
+  private assignPlayerIds() {
+    let westId, northId, eastId;
+    if (this.ownId == null) {
+      throw new Error("ownId is not defined");
+    }
+    const teams = this._store.get("teams");
+    // this hard coded for now, this could be a more sophisticated algorithm down the road if we want
+    const [ownTeam, otherTeam] = teams.sort((a, b) =>
+      a.includes(this.ownId!) ? -1 : 1
+    );
+    if (ownTeam[0] == this.ownId) {
+      northId = ownTeam[1];
+      westId = otherTeam[0];
+      eastId = otherTeam[1];
+    } else {
+      northId = ownTeam[0];
+      westId = otherTeam[1];
+      eastId = otherTeam[0];
+    }
+
+    this.otherPlayerWest.assignId(westId);
+    this.otherPlayerNorth.assignId(northId);
+    this.otherPlayerEast.assignId(eastId);
   }
 
   render() {
--- game/ui/scenes/game/OtherPlayer.ts
+++ game/ui/scenes/game/OtherPlayer.ts
@@ -9,11 +9,12 @@ import OtherHand, { OpponentPosition } from "./OtherHand";
 
 @injectable()
 class OtherPlayer {
-  _$infoContainer: HTMLDivElement;
-  _eventEmitter: EventEmitter;
-  _position: OpponentPosition | null = null;
-  _store: StoreType;
-  hand: OtherHand;
+  private _$infoContainer: HTMLDivElement;
+  private _eventEmitter: EventEmitter;
+  private _position: OpponentPosition | null = null;
+  private _store: StoreType;
+  private id: string | null = null;
+  private hand: OtherHand;
   constructor(position: OpponentPosition, store: StoreType, ee: EventEmitter) {
     this._position = position;
     this._$infoContainer = document.getElementById(
@@ -22,16 +23,32 @@ class OtherPlayer {
     this.hand = new OtherHand(position); // i could use inversify here, but not really important
     this._eventEmitter = ee;
     this._store = store;
-    this.initializeSubscriptions();
   }
   render() {
     this.hand.render();
   }
-  initializeSubscriptions() {
-    this._eventEmitter.addEventListener(LobbyEvents.TEAMS_RECEIVED, () => {
-      const ownId = this._store.get("ownId");
-    });
+
+  assignId(id: string) {
+    this.id = id;
+    this.showPlayerInfo();
   }
+
+  private showPlayerInfo() {
+    const data = this._store
+      .get("players")
+      .find((player) => player.id === this.id); // this should be rewritten to be dict lookup
+
+    if (!data) {
+      throw new Error(`Player data does not exist. Id : ${this.id}`);
+    }
+
+    const $nameEl = this._$infoContainer.querySelector("h5");
+    if ($nameEl) {
+      $nameEl.innerText = data.name;
+    }
+  }
+
+  initializeSubscriptions() {}
 }
 
 export default OtherPlayer;
